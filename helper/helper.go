57239c59c24f84669d675976252728ced53ef913
mayejacob <mayejacob3@gmail.com>
2023-11-17 01:30:29 +0100

    first commit

---
+++ helper/helper.go
@@ -0,0 +1,10 @@
+package helper
+
+import "strings"
+
+func ValidateInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isEmailValid := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	return isValidName, isEmailValid, isValidTicketNumber
+}
